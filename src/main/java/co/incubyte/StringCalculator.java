0641f52265fce70bee9e3873856073d5528bfa5b
Yatin Ajmani <yatinajmani@gmail.com>
2022-06-15 00:05:10 +0530

    Step 5 Completed

--- src/main/java/co/incubyte/StringCalculator.java
+++ src/main/java/co/incubyte/StringCalculator.java
@@ -3,10 +3,12 @@ package co.incubyte;
 import java.math.BigInteger;
 import java.util.Arrays;
 import java.util.List;
+import java.util.function.Supplier;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 class StringCalculator {
-    static BigInteger Add(String numbers) {
+    static BigInteger Add(String numbers) throws Exception {
         int doubleSlash = numbers.indexOf("//");
         Character delimiter = null;
         if (doubleSlash != -1) {
@@ -17,7 +19,12 @@ class StringCalculator {
             return BigInteger.ZERO;
         } else {
             String regex = (doubleSlash != -1 ? (delimiter + "|") : "") + ",|\n|\n\r";
-            List<BigInteger> integers = Arrays.stream(numbers.split(regex)).map(BigInteger::new).collect(Collectors.toList());
+            String finalNumbers = numbers;
+            Supplier<Stream<String>> stream = () -> Arrays.stream(finalNumbers.split(regex));
+            BigInteger negative = stream.get().map(BigInteger::new).filter(it -> it.signum() == -1).findFirst().orElse(BigInteger.ZERO);
+            if (negative.compareTo(BigInteger.ZERO) > 0)
+                throw new Exception("negatives not allowed - " + negative);
+            List<BigInteger> integers = stream.get().map(BigInteger::new).collect(Collectors.toList());
             BigInteger sum = BigInteger.ZERO;
             for (BigInteger integer : integers) {
                 sum = sum.add(integer);
--- src/test/java/co/incubyte/StringCalculatorTest.java
+++ src/test/java/co/incubyte/StringCalculatorTest.java
@@ -9,12 +9,17 @@ import java.util.stream.IntStream;
 
 public class StringCalculatorTest {
     @Test
-    public void testCalculator() {
+    public void testCalculator() throws Exception {
         Assert.assertEquals(BigInteger.ZERO, StringCalculator.Add("0"));
         Assert.assertEquals(BigInteger.ONE, StringCalculator.Add("1"));
         Assert.assertEquals(new BigInteger("3"), StringCalculator.Add("1,2"));
         Assert.assertEquals(new BigInteger("5050"), StringCalculator.Add(IntStream.rangeClosed(1, 100).boxed().map(Object::toString).collect(Collectors.joining(","))));
         Assert.assertEquals(new BigInteger("6"), StringCalculator.Add("1\n2,3"));
         Assert.assertEquals(new BigInteger("3"), StringCalculator.Add("//;\n1;2"));
+    }
+
+    @Test(expected = Exception.class)
+    public void negativeCase() throws Exception {
+        StringCalculator.Add("-1,1");
     }
 }
