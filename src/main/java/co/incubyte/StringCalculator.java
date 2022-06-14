19e4600da779e72232e3413abb397d2f6380cd10
Yatin Ajmani <yatinajmani@gmail.com>
2022-06-14 22:56:23 +0530

    Step 2 Completed

--- src/main/java/co/incubyte/StringCalculator.java
+++ src/main/java/co/incubyte/StringCalculator.java
@@ -1,13 +1,22 @@
 package co.incubyte;
 
+import java.math.BigInteger;
 import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
-public class StringCalculator {
-    public static int Add(String numbers) {
+class StringCalculator {
+    static BigInteger Add(String numbers) {
         if (numbers.equals("")) {
-            return 0;
+            return BigInteger.ZERO;
         } else {
-            return Arrays.stream(numbers.split(",")).mapToInt(Integer::valueOf).sum();
+            List<BigInteger> integers = Arrays.stream(numbers.split(",")).map(BigInteger::new).collect(Collectors.toList());
+            BigInteger sum = BigInteger.ZERO;
+            for (BigInteger integer : integers) {
+                sum = sum.add(integer);
+            }
+            return sum;
         }
     }
 }
--- src/test/java/co/incubyte/StringCalculatorTest.java
+++ src/test/java/co/incubyte/StringCalculatorTest.java
@@ -3,11 +3,16 @@ package co.incubyte;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.math.BigInteger;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
 public class StringCalculatorTest {
     @Test
     public void testCalculator() {
-        Assert.assertEquals(0, StringCalculator.Add("0"));
-        Assert.assertEquals(1, StringCalculator.Add("1"));
-        Assert.assertEquals(3, StringCalculator.Add("1,2"));
+        Assert.assertEquals(BigInteger.ZERO, StringCalculator.Add("0"));
+        Assert.assertEquals(BigInteger.ONE, StringCalculator.Add("1"));
+        Assert.assertEquals(new BigInteger("3"), StringCalculator.Add("1,2"));
+        Assert.assertEquals(new BigInteger("5050"), StringCalculator.Add(IntStream.rangeClosed(1, 100).boxed().map(Object::toString).collect(Collectors.joining(","))));
     }
 }
